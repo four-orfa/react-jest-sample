b8275d11b4fa1cddc990eb112fb1880ca10e31c5
Toshinori Sumida <t.sumida1987@gmial.com>
2021-11-07 20:56:15 +0900

    Redux test

---
+++ src/Redux.js
@@ -0,0 +1,24 @@
+import React from 'react'
+import { useSelector, useDispatch } from 'react-redux'
+import { selectCount, increment, decrement, incrementByAmount } from './features/customCounter/customCounterSlice'
+
+const Redux = () => {
+  const [number, setNumber] = React.useState(0)
+  const count = useSelector(selectCount)
+  const dispatch = useDispatch()
+
+  return (
+    <div>
+      <h3>Redux Integration Test</h3>
+      <div>
+        <button onClick={() => dispatch(increment())}>+</button>
+        <span data-testid='count-value'>{count}</span>
+        <button onClick={() => dispatch(decrement())}>-</button>
+        <button onClick={() => dispatch(incrementByAmount(number | 0))}>IncrementByAmount</button>
+      </div>
+      <input type='text' placeholder='Enter' onChange={(e) => setNumber(e.target.value)} />
+    </div>
+  )
+}
+
+export default Redux
